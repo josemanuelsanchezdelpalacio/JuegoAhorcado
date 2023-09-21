eae3d5ef1a18e5a71da7f051fa22121a7a891a12
josemanuelsanchezdelpalacio <170068@iessanalberto.com>
2023-09-21 18:30:28 +0200

    ejercicio ahorcado

--- app/src/main/java/com/damjms/iessanalberto/MainActivity.kt
+++ app/src/main/java/com/damjms/iessanalberto/MainActivity.kt
@@ -2,6 +2,7 @@ package com.damjms.iessanalberto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -16,7 +17,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var txtGameOver: TextView
 
     private var palabraSecreta = "Kotlin"
+    private var palabraMostrada = "*".repeat(palabraSecreta.length)
+    private var letrasNoEstan: MutableSet<Char> = mutableSetOf()
     private var numIntentos = 5
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -28,11 +32,51 @@ class MainActivity : AppCompatActivity() {
         edtLetrasNoEstan = findViewById(R.id.edtLetrasNoEstan)
         txtGameOver = findViewById(R.id.txtGameOver)
 
+        edtIntentos.setText(numIntentos.toString())
+        edtPalabra.setText(palabraMostrada)
+
+        btnComprobar.setOnClickListener {
+            val letra = edtIntroduceLetra.text.toString().uppercase().getOrNull(0)
+
+            if (letra != null) {
+                if (palabraSecreta.contains(letra)) {
+                    actualizarPalabraMostrada(letra)
+                } else {
+                    letrasNoEstan.add(letra)
+                    edtLetrasNoEstan.setText(letrasNoEstan.joinToString(" ") { "_" })
+                    decrementarIntento()
+                }
+            }
+
+            if (palabraMostrada == palabraSecreta) {
+                txtGameOver.text = "¡GANASTE!"
+                txtGameOver.visibility = View.VISIBLE
+                btnComprobar.isEnabled = false
+            }
+
+            edtIntroduceLetra.text.clear()
+        }
     }
 
-    fun adivinaPalabra() {
-        while(numIntentos > 0){
+    private fun actualizarPalabraMostrada(letra: Char) {
+        val nuevaPalabraMostrada = palabraMostrada.toCharArray()
+        for (i in palabraSecreta.indices) {
+            if (palabraSecreta[i] == letra) {
+                nuevaPalabraMostrada[i] = letra
+            }
+        }
+        palabraMostrada = nuevaPalabraMostrada.joinToString(" ")
+        edtPalabra.setText(palabraMostrada)
+    }
 
+    private fun decrementarIntento() {
+        numIntentos--
+        edtIntentos.setText(numIntentos.toString())
+
+        if (numIntentos == 0) {
+            txtGameOver.text = "GAME OVER. La palabra era: $palabraSecreta"
+            txtGameOver.visibility = View.VISIBLE
+            btnComprobar.isEnabled = false
         }
     }
 }
