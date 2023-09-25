923928a39f10cd2ac1c7db81cefea94f86e9026d
josemanuelsanchezdelpalacio <92978100+josemanuelsanchezdelpalacio@users.noreply.github.com>
2023-09-25 15:19:30 +0200

    ejercicio ahorcado

--- app/src/main/java/com/damjms/iessanalberto/MainActivity.kt
+++ app/src/main/java/com/damjms/iessanalberto/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    // Variables para los elementos de la interfaz
     private lateinit var edtIntentos: EditText
     private lateinit var edtIntroduceLetra: EditText
     private lateinit var btnComprobar: Button
@@ -16,8 +16,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var edtLetrasNoEstan: EditText
     private lateinit var txtGameOver: TextView
 
+    // Palabra secreta y estado actual mostrado al usuario
     private var palabraSecreta = "Kotlin"
     private var palabraMostrada = "*".repeat(palabraSecreta.length)
+
+    // Letras incorrectas y número de intentos
     private var letrasNoEstan: MutableSet<Char> = mutableSetOf()
     private var numIntentos = 5
 
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Inicialización de elementos de la interfaz
         edtIntentos = findViewById(R.id.edtIntentos)
         edtIntroduceLetra = findViewById(R.id.edtIntroduceLetra)
         btnComprobar = findViewById(R.id.btnComprobar)
@@ -32,10 +36,12 @@ class MainActivity : AppCompatActivity() {
         edtLetrasNoEstan = findViewById(R.id.edtLetrasNoEstan)
         txtGameOver = findViewById(R.id.txtGameOver)
 
+        // Configuración inicial de los elementos de la interfaz
         edtIntentos.setText(numIntentos.toString())
         edtPalabra.setText(palabraMostrada)
 
         btnComprobar.setOnClickListener {
+            // Obtención de la letra introducida por el usuario
             val letra = edtIntroduceLetra.text.toString().uppercase().getOrNull(0)
 
             if (letra != null) {
@@ -49,6 +55,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             if (palabraMostrada == palabraSecreta) {
+                // Mostrar mensaje de victoria si se adivina la palabra
                 txtGameOver.text = "¡GANASTE!"
                 txtGameOver.visibility = View.VISIBLE
                 btnComprobar.isEnabled = false
@@ -59,6 +66,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun actualizarPalabraMostrada(letra: Char) {
+        // Actualizar la palabra mostrada al usuario con la letra adivinada
         val nuevaPalabraMostrada = palabraMostrada.toCharArray()
         for (i in palabraSecreta.indices) {
             if (palabraSecreta[i] == letra) {
@@ -70,10 +78,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun decrementarIntento() {
+        // Decrementar el número de intentos y actualizar la interfaz
         numIntentos--
         edtIntentos.setText(numIntentos.toString())
 
         if (numIntentos == 0) {
+            // Mostrar mensaje de fin de juego si se agotan los intentos
             txtGameOver.text = "GAME OVER. La palabra era: $palabraSecreta"
             txtGameOver.visibility = View.VISIBLE
             btnComprobar.isEnabled = false
